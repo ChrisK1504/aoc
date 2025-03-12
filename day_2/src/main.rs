13657bdc95f424120824c2354529327d2661fdec
ChrisK1504 <krismehilli@yahoo.com>
2025-03-12 15:16:33 +0100

    Day_2: Task 1 Complete;

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,5 +1,34 @@
+use std::cmp;
 use std::env;
 use std::fs;
+
+pub fn solve(input: String) -> u32 {
+    let mut total_paper = 0;
+    for line in input.lines() {
+        let mut dimensions = line
+            .split('x')
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+
+        dimensions.sort_unstable();
+
+        if dimensions.len() != 3 {
+            continue;
+        }
+
+        let l = dimensions[0];
+        let w = dimensions[1];
+        let h = dimensions[2];
+
+        let wrapping_paper = 2 * l * w + 2 * w * h + 2 * h * l;
+
+        let slack = cmp::min(l * w, cmp::min(l * h, w * h));
+
+        total_paper += wrapping_paper + slack;
+    }
+
+    return total_paper;
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -7,5 +36,6 @@ fn main() {
     let mut input = fs::read_to_string(file_path).expect("Couldn't read the file");
 
     input = input.trim().to_string();
-    println!("Content: {input}");
+    let answer = solve(input);
+    println!("{answer}");
 }
